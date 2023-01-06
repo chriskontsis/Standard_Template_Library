bcc3d1ca50222414529a9191be76148b7cb07e3a
chriskontsis <cak0927@gmail.com>
2023-01-06 16:45:20 -0700

    continuing vector

--- Containters/vector.cc
+++ Containters/vector.cc
@@ -1,6 +1,6 @@
+#include <iostream>
+
 using namespace std;
-
-
 template<class T> 
 class Vector {
 private:
@@ -8,20 +8,50 @@ private:
     unsigned int my_capacity;
     T* buffer;
 public:
-    Vector();
-    Vector(unsigned int size);
-    Vector(unsigned int size, const T& inital);
-    Vector(const Vector<T> & v);
-    ~Vector();
+    Vector() : my_size(0), my_capacity(0), buffer(0) {};
+    Vector(unsigned int size): my_size(size), my_capacity(size), buffer(new T[size]) {};
 
-    unsigned int capatity() const;
-    unsigned int size() const;
-    bool empty() const;
-    T* begin();
-    T* end();
-    T& front();
-    T& back();
-    void push_back(const T& value);
+    Vector(unsigned int size, const T& initial) : my_size(size), my_capacity(size), buffer(new T[size]) {
+        for(auto i = 0; i < size; i++) 
+            buffer[i] = initial;
+    } 
+
+    Vector(const Vector<T> & v) : my_size(v.my_size), my_capacity(v.my_capacity), buffer(new T[v.my_size]) {
+        for(auto i = 0; i < size; i++) 
+            buffer[i] = v.buffer[i];
+    }
+    ~Vector() {
+        delete[] buffer;
+    }
+
+    unsigned int capatity() const {
+        return my_capacity;
+    }
+
+    unsigned int size() const {
+        return my_size;
+    }
+
+    bool empty() const {};
+
+    T* begin() {
+        return buffer;
+    }
+    T* end() {
+        return buffer + my_size;
+    }
+
+    T& front() {
+        return buffer[0];
+    }
+    T& back() {
+        return buffer[my_size -1];
+    }
+
+    void push_back(const T& value) {
+
+    }
+    
     void pop_back();
     void reserve(unsigned int capacity);
     void resize (unsigned int size);
@@ -30,4 +60,9 @@ public:
     Vector<T> & operator=(const Vector<T>& vec);
     void clear();
 };
+
+int main() {
+    Vector<int> vec(10,1);
+
+}
 
