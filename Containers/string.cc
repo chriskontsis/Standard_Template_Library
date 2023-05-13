98da5f672e4c2105763412e2171f8710a8af2f2c
chriskontsis <cak0927@gmail.com>
2023-05-13 16:13:36 -0700

    more string stuff

--- Containers/string.cc
+++ Containers/string.cc
@@ -18,15 +18,43 @@ public:
     // Copy Constructor
     String(const String& other) : String(other.s_data) {}
 
+    // Move Constructor
     String(String&& other) noexcept : s_length(0), s_data(nullptr) {
         swap(*this, other);
     }
 
+    //Destructor
+    ~String() {
+        delete[] s_data;
+    }
 
+    //Copy Assignment
+    String& operator=(const String& other) {
+        String temp(other);
+        swap(*this, temp);
+        return *this;
+    }
+    // Move Assignment
+    String& operator=(String&& other) {
+        swap(*this, other);
+        return *this;
+    }
 
+    int length() const {
+        return s_length;
+    }
+
+    char& operator[](int idx) {
+        return s_data[idx];
+    }
+
+    // declared friend because it needs to access private members of string class
+    // swap is not a member function of the string class so it can't access them otherwise
     friend void swap(String& a, String& b) {
         using std::swap;
         swap(a.s_data, b.s_data);
         swap(a.s_length, b.s_length);
     }
+
+    friend std::ostream& operator<<(std::ostream& os, const String& str);
 };
