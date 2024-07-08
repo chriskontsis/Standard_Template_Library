3cc67e4c5ab8651bcbf8d34588ccc658b0bd38d4
chriskontsis <cak0927@gmail.com>
2024-07-08 13:49:12 -0400

    operator +

--- String/string.cc
+++ String/string.cc
@@ -9,12 +9,14 @@ class String {
         String() 
         : data {nullptr}, length {0} 
         {
+            ++cnt;
             std::cout << "String()\n";
         }
 
         String(const char* other) 
         : length(std::strlen(other)) 
         {
+            ++cnt;
             std::cout << "String(const char*)\n";
             data = new char[length + 1];
             std::copy(other, other+length+1, data);
@@ -23,6 +25,7 @@ class String {
         String(const String& other) 
         : length {other.length}
         {
+            ++cnt;
             std::cout << "String(const String& other)\n";
             data = new char[length + 1];
             std::copy(other.data, other.data+other.length+1, data);
@@ -31,6 +34,7 @@ class String {
         String(String&& other) noexcept 
         : data{other.data}, length {other.length}
         {
+            ++cnt;
             std::cout << "String(String&& other)\n";
             other.data = nullptr;
             other.length = 0;
@@ -57,6 +61,26 @@ class String {
             return std::strcmp(data, rhs.data) == 0;
         }
 
+        String operator+(const String &rhs) { 
+            std::cout << "String operator+(const String& rhs)\n";
+            size_t newLen = length + rhs.length + 1;
+            char *newdata = new char[newLen];
+            strcpy(newdata, data);
+            strcat(newdata, rhs.data);
+            String result(newdata);
+            delete[] newdata;
+            return result;
+        }
+
+
+
+        const char& operator[](std::size_t idx) const
+        {
+            if(idx < 0 || idx >= length)
+                throw std::out_of_range("Idx out of bounds\n");
+            return data[idx];
+        }
+
         friend std::ostream& operator<<(std::ostream& os, String& str)
         {
             os << str.data;
@@ -73,8 +97,11 @@ class String {
     private:
         char* data;
         std::size_t length;
+    public:
+        static int cnt;
 };
 
+int String::cnt = 0;
 
 int main() {
     // Default ctor
@@ -103,11 +130,14 @@ int main() {
     std::cout << std::boolalpha;
     std::cout << (s7 == s6) << '\n';
     String s8 = "World";
-    std::cout << (s7 == s8) << '\n';
+    std::cout << (s7 == s8) << "\n\n";
     
     // operator+
+    String s9 = s2 + s8;
+    std::cout << s9 << '\n';
 
     // operator+=
 
     std::cout << '\n';
+    std::cout << String::cnt << '\n';
 }
